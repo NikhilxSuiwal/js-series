03427881e4b7c7ff5072b4ff0e5d6c25491ec404
Nikhil Suiwal <140813563+NikhilxSuiwal@users.noreply.github.com>
2025-01-17 17:30:42 +0000

    conversion of datatypes done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true/false => 1/0
+// null => 0
+
+let isLoggedIn = 10
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 0/1 => false/true
+// "" => false
+// "nikhil" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
