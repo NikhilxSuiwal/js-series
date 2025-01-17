6e0912b4cd194bf35bb610b14bbab34db105fea0
Nikhil Suiwal <140813563+NikhilxSuiwal@users.noreply.github.com>
2025-01-17 17:51:06 +0000

    conversion opeartion done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -23,8 +23,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************** OPERATIONS ******************
+
+let value = 9
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  // ye power m use aata h i mean 2 raise to power 2
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Nikk"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+
+
 
 
 
