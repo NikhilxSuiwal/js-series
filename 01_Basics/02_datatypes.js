c7a7ecef1f94e635aaadd22ca62685ad0ff62426
Nikhil Suiwal <140813563+NikhilxSuiwal@users.noreply.github.com>
2025-01-17 17:03:25 +0000

    datatypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // Treat all js code as newer version
+
+//alert(3+3) // Give error because we are using nodejs not browser
+
+// Primitive Datatypes :-
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => jiski abhi value asign nhi hui h
+// symbol => unique
+
+// object
+
+console.log(typeof null); // it will give object 
+
