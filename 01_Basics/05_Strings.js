f715f94a1918b1c6a0b3e5b1a4fd967582044928
Nikhil Suiwal <140813563+NikhilxSuiwal@users.noreply.github.com>
2025-01-18 10:46:52 +0000

    String done

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,15 @@
+const name = "Nikhil"
+const repoCount = 50
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Hitesh')
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('k'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
