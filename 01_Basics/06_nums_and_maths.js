9a019b8617ef9a5246c010d9c10be58b8bab3c07
Nikhil Suiwal <140813563+NikhilxSuiwal@users.noreply.github.com>
2025-01-18 11:25:48 +0000

    numbers and maths done

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(3))
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-in'))
+
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-10))
+// console.log(Math.round(10.543))
+// console.log(Math.sqrt(4))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10)+1))
+
+const min = 10
+const max= 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
